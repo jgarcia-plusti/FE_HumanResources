fdaf3ecd8cf654f1677d882b73431c5834345457
oscarleon <oscarleon.alcatel@gmai.com>
2024-03-08 11:03:38 -0600

    configuración de los destinos CSS para el login

--- obj/Debug/net8.0/FE_HumanResources.AssemblyInfo.cs
+++ obj/Debug/net8.0/FE_HumanResources.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FE_HumanResources")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ea8ca6143475034103b66f66e7d6e27490d12af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824e3a8e934cc47fe7129fb58336bb3ea67a13dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FE_HumanResources")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FE_HumanResources")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
