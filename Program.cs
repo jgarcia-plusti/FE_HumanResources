304fde9c4ebb374b48f76ae96b089cc3fc26ee36
Josué García <jgarcia@plusti.com>
2024-03-14 13:24:15 -0600

    Modificaciones de integracion de Radzen Blazor

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using FE_HumanResources;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,12 +15,15 @@ builder.Services.AddScoped(sp =>
     {
         BaseAddress = new Uri("https://prodisarh.com:444")
     });
-
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredToast();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<DialogService>();
 
 
 await builder.Build().RunAsync();
