4f7f1863ca3b66410d8668aeeda68e49cf226bd9
Josué García <jgarcia@plusti.com>
2024-03-15 13:55:43 -0600

    Formulario de Usuarios Radzen.Blazor

---
+++ Entities/RoleEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace FE_HumanResources.Entities
+{
+    public class RoleEntity
+    {
+        public string Uuid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
