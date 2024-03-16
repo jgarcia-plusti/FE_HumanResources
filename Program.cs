82414eec211c0de6299ecab235db8dfb5b795306
Josué García <jgarcia@plusti.com>
2024-03-15 18:29:55 -0600

    Cambios para layout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using FE_HumanResources;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
@@ -18,6 +19,8 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+builder.Services.AddRadzenComponents();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddScoped<TooltipService>();
