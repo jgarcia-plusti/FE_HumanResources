8da0993db8b53da18804af42ed687bc73274fb93
oscarleon <oscarleon.alcatel@gmai.com>
2024-03-18 15:43:54 -0600

    topmenu

--- obj/Debug/net8.0/FE_HumanResources.AssemblyInfo.cs
+++ obj/Debug/net8.0/FE_HumanResources.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FE_HumanResources")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56947138d58930a8a0bad1174eb97fef08a992d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a961686eaa6f333f9d92f273540a817c4ee6b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("FE_HumanResources")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FE_HumanResources")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
