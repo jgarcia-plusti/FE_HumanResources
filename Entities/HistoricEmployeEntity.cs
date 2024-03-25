6deb298d5cf29ee64c78b7af63612fd05fc57fca
Josué García <jgarcia@plusti.com>
2024-03-25 11:14:44 -0600

    Historico de Empleados

---
+++ Entities/HistoricEmployeEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace FE_HumanResources.Entities
+{
+    public class HistoricEmployeEntity
+    {
+        public string Uuid { get; set; }
+        public string UuidUser { get; set; }
+        public string uuidEmployee { get; set; }
+        public string ActionType { get; set; }
+        public string Module { get; set; }
+        public string OldRegister { get; set; }
+        public string NewRegister { get; set; }
+        public string NameEmployee { get; set; }
+        public string UserName { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
